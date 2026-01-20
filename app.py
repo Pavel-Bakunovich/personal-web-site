1f25419ad82c537e08a855f3a9aaa14345590148
Pavel Bakunovich <Pavel-Bakunovich@users.noreply.github.com>
2026-01-20 06:54:22 +0000

    Implemented "Download CV" button using weasyprint

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, make_response
+import io
+from weasyprint import HTML
 
 app = Flask(__name__)
 
@@ -13,6 +15,21 @@ def cv():
 @app.route('/my-story')
 def my_story():
     return render_template('my-story.html')
+
+@app.route('/download-cv')
+def download_cv():
+    # Render the CV template
+    html = render_template('cv.html')
+    
+    # Create PDF in memory
+    pdf = HTML(string=html).write_pdf()
+    
+    # Create response
+    response = make_response(pdf)
+    response.headers['Content-Type'] = 'application/pdf'
+    response.headers['Content-Disposition'] = 'attachment; filename=CV Pavel Bakunovich.pdf'
+    
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
